7b4f5ac1ed9cbd8d458ec5a2d2a9aaf311302cad
Eric J. Holmes <eric@ejholmes.net>
2014-07-03 14:42:06 -0700

    doc.

--- metrics.go
+++ metrics.go
@@ -17,18 +17,22 @@ var Logger = log.New(os.Stdout, "", 0)
 // The root namespace.
 var root Namespace = ""
 
+// Count logs a count metric.
 func (n Namespace) Count(metric string, v interface{}) {
 	n.print(MetricCount, metric, v, "")
 }
 
+// Sample logs a sample metric.
 func (n Namespace) Sample(metric string, v interface{}, units string) {
 	n.print(MetricSample, metric, v, units)
 }
 
+// Measure logs a measurement metric.
 func (n Namespace) Measure(metric string, v interface{}, units string) {
 	n.print(MetricMeasure, metric, v, units)
 }
 
+// print prints a metric type to the logger.
 func (n Namespace) print(t MetricType, metric string, v interface{}, units string) {
 	if n != "" {
 		metric = fmt.Sprintf("%s.%s", n, metric)
@@ -37,14 +41,17 @@ func (n Namespace) print(t MetricType, metric string, v interface{}, units strin
 	m.Print()
 }
 
+// Count logs a count metric in the root namespace.
 func Count(metric string, v interface{}) {
 	root.Count(metric, v)
 }
 
+// Sample logs a sample metric in the root namespace.
 func Sample(metric string, v interface{}, units string) {
 	root.Sample(metric, v, units)
 }
 
+// Measure logs a measurement metric in the root namespace.
 func Measure(metric string, v interface{}, units string) {
 	root.Measure(metric, v, units)
 }
