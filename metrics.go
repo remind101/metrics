a083cf5d73ee30e5820555b0afc7db9bb0b5beb2
Eric J. Holmes <eric@ejholmes.net>
2014-07-03 14:27:32 -0700

    Namespace.

--- metrics.go
+++ metrics.go
@@ -3,27 +3,48 @@
 package metrics
 
 import (
+	"fmt"
 	"log"
 	"os"
 )
+
+type Namespace string
 
 // Logger is the logger to use when printing metrics. By default, metrics are printed
 // to Stdout.
 var Logger = log.New(os.Stdout, "", 0)
 
+// The root namespace.
+var root Namespace = ""
+
+func (n Namespace) Count(metric string, v interface{}) {
+	n.print(MetricCount, metric, v, "")
+}
+
+func (n Namespace) Sample(metric string, v interface{}, units string) {
+	n.print(MetricSample, metric, v, units)
+}
+
+func (n Namespace) Measure(metric string, v interface{}, units string) {
+	n.print(MetricMeasure, metric, v, units)
+}
+
+func (n Namespace) print(t MetricType, metric string, v interface{}, units string) {
+	if n != "" {
+		metric = fmt.Sprintf("%s.%s", n, metric)
+	}
+	m := &Metric{Name: metric, Type: t, Value: v, Units: units}
+	m.Print()
+}
+
 func Count(metric string, v interface{}) {
-	printMetric(MetricCount, metric, v, "")
+	root.Count(metric, v)
 }
 
 func Sample(metric string, v interface{}, units string) {
-	printMetric(MetricSample, metric, v, units)
+	root.Sample(metric, v, units)
 }
 
 func Measure(metric string, v interface{}, units string) {
-	printMetric(MetricMeasure, metric, v, units)
-}
-
-func printMetric(t MetricType, metric string, v interface{}, units string) {
-	m := &Metric{Name: metric, Type: t, Value: v, Units: units}
-	m.Print()
+	root.Measure(metric, v, units)
 }
